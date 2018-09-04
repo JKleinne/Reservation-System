03cf04255152dc4fc30bc5b8db02807c7dce0e95
Jonni Quezada <jonniequezada@gmail.com>
2018-09-04 14:12:38 -0400

    Added database files

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../stylesheets/App.css';
 
 class App extends Component {
   render() {
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Hi</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <code>Hello There</code>
         </p>
       </div>
     );
