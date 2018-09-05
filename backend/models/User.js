2a71576adc714fa21336dbe05b2ad9084bb691a4
Jonni Quezada <jonniequezada@gmail.com>
2018-09-04 21:47:24 -0400

    Started models and provided examples

---
+++ backend/models/User.js
@@ -0,0 +1,33 @@
+const db = require('../database/database');
+
+/*
+ * Add operation so use queryTransaction
+ */
+async function addUser(name, phone, email, studentId) {
+    const cmd = 'INSERT INTO User (...)';
+
+    try {
+        let connection = await db.openConnection();
+        await db.queryTransaction(cmd, connection);
+        connection.release();
+    } catch(error) {
+        console.error(error);
+    }
+}
+
+/*
+ * Fetch operation so use query
+ */
+async function getUserById(studentId) {
+    const cmd = `SELECT * FROM User WHERE studentId = ${studentId}`;
+
+    try {
+        return await db.query(cmd);
+    } catch(error) {
+        console.error(error);
+    }
+}
+
+module.exports = {
+    addUser
+};
