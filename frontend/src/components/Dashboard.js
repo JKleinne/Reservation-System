d15bf0ff3730906e3339aa1fc4990537cd0b28d6
Jonnie Quezada <jonniequezada@gmail.com>
2018-11-19 15:42:21 -0500

    Login Page

---
+++ frontend/src/components/Dashboard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import logo from '../logo.svg';
+import '../stylesheets/login-signup.css';
+
+class Dashboard extends Component {
+    render() {
+        return (
+           <div>
+
+           </div>
+        );
+    }
+}
+
+export default Dashboard;
